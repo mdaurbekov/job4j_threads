4d0ad41aec4f450b8c5460544af4d5a7f2fe440a
mdaurbekov <dreamkov@gmail.com>
2022-10-30 15:00:24 +0300

    threads

---
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -0,0 +1,23 @@
+package ru.job4j.concurrent;
+
+public class ThreadState {
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> {}, "first"
+        );
+        Thread second = new Thread(
+                () -> {}, "second"
+        );
+        System.out.println("Нить: " + first.getName() + " - " + first.getState());
+        System.out.println("Нить: " + second.getName() + " - " + second.getState());
+        first.start();
+        second.start();
+        while (first.getState() != Thread.State.TERMINATED && second.getState() != Thread.State.TERMINATED) {
+            System.out.println("Нить: " + first.getName() + " - " + first.getState());
+            System.out.println("Нить: " + second.getName() + " - " + second.getState());
+
+        }
+        System.out.println("Нить: " + first.getName() + " - " + first.getState());
+        System.out.println("Нить: " + second.getName() + " - " + second.getState());
+    }
+}

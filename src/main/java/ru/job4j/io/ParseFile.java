3327f97231300c16fbaa0c6197db7495f07ad287
mdaurbekov <dreamkov@gmail.com>
2022-11-01 20:24:47 +0300

    threads

---
+++ src/main/java/ru/job4j/io/ParseFile.java
@@ -0,0 +1,43 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.util.function.Predicate;
+
+public final class ParseFile {
+    private final File file;
+
+    public ParseFile(File file) {
+        this.file = file;
+    }
+
+    public synchronized File getFile() {
+        return file;
+    }
+
+    public String getContent(Predicate<Character> filter) throws IOException {
+        try (BufferedReader in = new BufferedReader(new FileReader(file))) {
+            String output = "";
+            int data;
+            while ((data = in.read()) > 0) {
+                char s = (char) data;
+                if (filter.test(s)) {
+                    output += s;
+                }
+            }
+            return output;
+        }
+
+    }
+
+
+    public final class SaveFile {
+
+        public void saveContent(String content) throws IOException {
+            try (PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(file)))) {
+                out.write(content);
+            }
+
+        }
+    }
+
+}

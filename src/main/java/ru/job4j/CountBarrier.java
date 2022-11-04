9483d45b103427f8008154d56b41fc4275878b7d
mdaurbekov <dreamkov@gmail.com>
2022-11-04 17:49:53 +0300

    threads

--- src/main/java/ru/job4j/CountBarrier.java
+++ src/main/java/ru/job4j/CountBarrier.java
@@ -21,7 +21,7 @@ public class CountBarrier {
 
     public void await() {
         synchronized (monitor) {
-            while (count <= total) {
+            while (count < total) {
                 try {
                     monitor.wait();
                 } catch (InterruptedException e) {

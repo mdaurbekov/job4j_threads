2fa7fd873c43605343b19c7755cbd5c331910c5d
mdaurbekov <dreamkov@gmail.com>
2022-11-05 10:13:43 +0300

    threads

---
+++ src/main/java/ru/job4j/buffer/ParallelSearch.java
@@ -0,0 +1,46 @@
+package ru.job4j.buffer;
+
+import ru.job4j.synch.SimpleBlockingQueue;
+
+public class ParallelSearch {
+
+    public static void main(String[] args) throws InterruptedException {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<Integer>(3);
+        final Thread consumer = new Thread(
+                () -> {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        try {
+                            System.out.println(queue.poll());
+
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+
+
+                    }
+
+                }
+        );
+        consumer.start();
+
+
+        new Thread(
+                () -> {
+                    for (int index = 0; index != 3; index++) {
+                        try {
+                            queue.offer(index);
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                    consumer.interrupt();
+
+                }
+
+        ).start();
+
+
+    }
+}

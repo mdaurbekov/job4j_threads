4ae80f1640ff0cc22c6bef71287f411e7adf2f62
mdaurbekov <dreamkov@gmail.com>
2022-11-12 20:45:18 +0300

    threads

--- src/main/java/ru/job4j/pool/EmailNotification.java
+++ src/main/java/ru/job4j/pool/EmailNotification.java
@@ -10,12 +10,7 @@ public class EmailNotification {
     public void emailTo(User user) {
         String subject = String.format("Notification %s to email %s", user.getUsername(), user.getEmail());
         String body = String.format("Add a new event to %s", user.getUsername());
-        pool.submit(new Runnable() {
-            @Override
-            public void run() {
-                send(subject, body, user.getEmail());
-            }
-        });
+        pool.submit(() -> send(subject, body, user.getEmail()));
     }
 
     public void close() {
@@ -38,12 +33,9 @@ public class EmailNotification {
 
     public static void main(String[] args) {
         EmailNotification emailNotification = new EmailNotification();
-        emailNotification.pool.submit(new Runnable() {
-            @Override
-            public void run() {
-                User user = new User("Ivan", "email@mail.ru");
-                emailNotification.emailTo(user);
-            }
+        emailNotification.pool.submit(() -> {
+            User user = new User("Ivan", "email@mail.ru");
+            emailNotification.emailTo(user);
         });
 
         emailNotification.close();
